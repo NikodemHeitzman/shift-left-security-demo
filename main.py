2dcc0a2a84497f35fd2a9180c7eb6347754c49a5
NikodemHeitzman <nikodem.heitzman2002@gmail.com>
2026-03-10 19:43:47 +0100

    feat: init app and setup local security guards

---
+++ main.py
@@ -0,0 +1,42 @@
+import sqlite3
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+def init_db():
+    conn = sqlite3.connect("test.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+                   CREATE TABLE IF NOT EXISTS users (
+                                                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                                        username VARCHAR(50) NOT NULL UNIQUE,
+                       secret_data TEXT NOT NULL
+                       )
+                   """)
+
+    cursor.execute("INSERT OR IGNORE INTO users(username, secret_data) VALUES (?, ?)", ("Admin", "HardPassword"))
+    cursor.execute("INSERT OR IGNORE INTO users(username, secret_data) VALUES (?, ?)", ("Steven_Brand", "EZPassword"))
+
+    conn.commit()
+    conn.close()
+
+init_db()
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/users/{username}")
+async def get_user(username: str):
+    conn = sqlite3.connect("test.db")
+    cursor = conn.cursor()
+    query = f"SELECT * FROM users WHERE username = '{username}'"
+    cursor.execute(query)
+    result = cursor.fetchone()
+    return {"username": result}
+
+@app.get("/search", response_class=HTMLResponse)
+async def search(query: str):
+    return f"<h1>Search results for: {query}</h1>"
